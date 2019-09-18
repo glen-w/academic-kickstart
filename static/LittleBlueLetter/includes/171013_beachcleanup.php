5fb98ed9dbead707f3d79b13a1032c5aecc13483
glen-w <6459622+glen-w@users.noreply.github.com>
2019-09-19 01:43:24 +0200

    Add files via upload

---
+++ static/LittleBlueLetter/includes/171013_beachcleanup.php
@@ -0,0 +1,13 @@
+<div align="center">
+<p>Our Ocean 2017 Youth Summit, Malta - beach cleanup</p>
+<table cellspacing="3">
+  <tr>
+    <td><a href="../images/171013_cleanup1.jpg"><img src="../images/171013_cleanup1.jpg" width="175"></a></td>
+    <td><a href="../images/171013_cleanup2.jpg"><img src="../images/171013_cleanup2.jpg" width="175"></a></td>
+  </tr>
+  <tr>
+    <td><a href="../images/171013_cleanup4.jpg"><img src="../images/171013_cleanup4.jpg" width="175"></a></td>
+    <td><a href="../images/171013_cleanup3.jpg"><img src="../images/171013_cleanup3.jpg" width="175"></a></td>
+  </tr>
+</table>
+</div>
---
+++ static/LittleBlueLetter/includes/171013_whale.php
@@ -0,0 +1,13 @@
+<div align="center">
+<p>Our Ocean 2017 - plastic debris whale</p>
+<table cellspacing="3">
+  <tr>
+    <td><a href="../images/171013_whale1.jpg"><img src="../images/171013_whale1.jpg" width="175"></a></td>
+    <td><a href="../images/171013_whale2.jpg"><img src="../images/171013_whale2.jpg" width="175"></a></td>
+  </tr>
+  <tr>
+    <td><a href="../images/171013_whale3.jpg"><img src="../images/171013_whale3.jpg" width="175"></a></td>
+    <td><a href="../images/171013_whale4.jpg"><img src="../images/171013_whale4.jpg" width="175"></a></td>
+  </tr>
+</table>
+</div>
